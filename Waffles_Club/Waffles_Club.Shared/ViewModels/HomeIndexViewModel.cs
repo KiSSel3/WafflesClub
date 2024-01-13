1af8d5a8c52fc63e333715bb25ef4e7cf85b7cf4
KiSSel3 <andreyk042004@gmail.com>
2024-01-13 20:45:33 +0300

    Update config

---
+++ Waffles_Club/Waffles_Club.Shared/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Waffles_Club.Data.Entity;
+using Waffles_Club.Shared.Helpers;
+
+namespace Waffles_Club.Shared.ViewModels
+{
+	public class HomeIndexViewModel
+	{
+		public PaginatedList<Waffle> Waffles { get; set; }
+		public SelectList SelectListWaffleType { get; set; }
+		public SelectList SelectListFillingType { get; set; }
+	}
+}
--- Waffles_Club/Waffles_Club/Controllers/WaffleController.cs
+++ Waffles_Club/Waffles_Club/Controllers/WaffleController.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Waffles_Club.Service.Services.Interfaces;
+using Waffles_Club.Shared.ViewModels;
 
 namespace Waffles_Club.Controllers
 {
 	public class WaffleController : Controller
 	{
+		private readonly IWaffleService _waffleService;
+
+		private readonly IConfiguration _configuration;
+
+		public WaffleController(IWaffleService waffleService, IConfiguration configuration)
+		{
+			_waffleService = waffleService;
+			_configuration = configuration;
+		}
+
+		[HttpGet]
 		public IActionResult Index()
 		{
 			return View();
