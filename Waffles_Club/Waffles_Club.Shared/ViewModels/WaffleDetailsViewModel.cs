b0ad705958e8e280f44837a024cd3b831a9f6f01
KiSSel3 <andreyk042004@gmail.com>
2024-01-16 14:45:09 +0300

    Waffle Details added

---
+++ Waffles_Club/Waffles_Club.Shared/ViewModels/WaffleDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Waffles_Club.Data.Entity;
+
+namespace Waffles_Club.Shared.ViewModels
+{
+    public class WaffleDetailsViewModel
+    {
+        public Waffle Waffle { get; set; }
+        public WaffleType WaffleType { get; set; }
+        public FillingType FillingType { get; set; }
+    }
+}
