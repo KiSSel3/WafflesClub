1717fb90dfef04f519598b876f56319f42aca02d
KiSSel3 <andreyk042004@gmail.com>
2024-01-16 17:02:38 +0300

    Update layout

---
+++ Waffles_Club/Waffles_Club/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Waffles_Club.Service.Services.Interfaces;
+
+namespace Waffles_Club.Components.Cart
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+        
+        public CartViewComponent(ICartService cartService) =>
+            _cartService = cartService;
+
+        public async Task<IViewComponentResult> InvokeAsync(string userId)
+        {
+            try
+            {
+                var cart = await _cartService.GetByUserIdAsync(userId);
+                return View("CartView", cart.Count());
+            }
+            catch
+            {
+                return View("CartView", 0);
+            }
+        }
+    }
+}
