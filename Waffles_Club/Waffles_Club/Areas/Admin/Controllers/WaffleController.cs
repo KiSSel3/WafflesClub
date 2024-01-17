e0ed8012ecdcd8b44b0819e3634f7ada59b28b66
Alexey-beercode <alsemkovbn@gmail.com>
2024-01-17 23:13:29 +0300

    Updated controller

--- Waffles_Club/Waffles_Club/Areas/Admin/Controllers/WaffleController.cs
+++ Waffles_Club/Waffles_Club/Areas/Admin/Controllers/WaffleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Waffles_Club.Data.Entity;
 using Waffles_Club.Service.Services.Interfaces;
 using Waffles_Club.Shared.ViewModels;
 
@@ -9,17 +10,29 @@ namespace Waffles_Club.Areas.Admin.Controllers
     public class WaffleController : Controller
     {
         private readonly IWaffleService _waffleService;
+        private readonly IFillingTypeService _fillingTypeService;
+        private readonly IWaffleTypeService _waffleTypeService;
 
-        public WaffleController(IWaffleService waffleService)
+        public WaffleController(IWaffleService waffleService, IWaffleTypeService waffleTypeService, IFillingTypeService fillingTypeService)
         {
             _waffleService = waffleService;
+            _waffleTypeService = waffleTypeService;
+            _fillingTypeService = fillingTypeService;
+        }
+        private async Task UpdateViewBag()
+        {
+            var fillingTypes = await _fillingTypeService.GetAllAsync();
+            var waffleTypes = await _waffleTypeService.GetAllAsync();
+            ViewBag.FillingTypes = fillingTypes;
+            ViewBag.WafflesTypes = waffleTypes;
         }
         [Authorize]
         public async Task<IActionResult> GetAllWaffles()
         {
             try
             {
-                var waffles= await _waffleService.GetWaffleListAsync();
+                await UpdateViewBag();
+                var waffles = await _waffleService.GetWaffleListAsync();
                 return View("Waffles", waffles);
             }
             catch (Exception ex)
@@ -33,6 +46,7 @@ namespace Waffles_Club.Areas.Admin.Controllers
         {
             try
             {
+                await UpdateViewBag();
                 var waffle= await _waffleService.GetWaffleByIdAsync(waffleId);
                 return View("Edit", waffle);
             }
@@ -43,7 +57,13 @@ namespace Waffles_Club.Areas.Admin.Controllers
         }
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult> Update()
+        public async Task<IActionResult> Update(WaffleViewModel viewModel,Guid waffleId)
+        {
+            await _waffleService.UpdateWaffleAsync(waffleId,viewModel);
+            var waffles = await _waffleService.GetWaffleListAsync();
+            return View("Waffles", waffles);
+        }
+
         [Authorize]
         [HttpGet]
         public async Task<IActionResult> Create()
