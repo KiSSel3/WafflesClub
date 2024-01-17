6877fc8ad6b7bc89e4c0da227b5747e6d33fc5a8
Alexey-beercode <alsemkovbn@gmail.com>
2024-01-17 21:48:54 +0300

    update controller, added views

---
+++ Waffles_Club/Waffles_Club/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Waffles_Club.Service.Services.Interfaces;
+using Waffles_Club.Shared.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Waffles_Club.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var users = await _userService.GetAll();
+                return View("Users", users);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new ErrorViewModel() { RequestId = ex.Message });
+            }
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid userId)
+        {
+            try
+            {
+                var user = await _userService.GetById(userId.ToString());
+                return View("Edit", user);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new ErrorViewModel() { RequestId = ex.Message });
+            }
+        }
+    }
+}
