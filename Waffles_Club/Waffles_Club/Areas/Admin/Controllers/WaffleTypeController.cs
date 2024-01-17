86f4c38d3bfd4100266198e5aba3314f71cd7a52
KiSSel3 <andreyk042004@gmail.com>
2024-01-18 00:17:13 +0300

    Create waffletype controller

---
+++ Waffles_Club/Waffles_Club/Areas/Admin/Controllers/WaffleTypeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Waffles_Club.Areas.Admin.Controllers
+{
+    public class WaffleTypeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
